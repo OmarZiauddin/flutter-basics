ace2c8c67f4a96d29e7ab242ee0104de12588f73
Omar Zia ud Din <omarghafoorjan@gmail.com>
2022-10-31 12:27:19 +0500

    Day 2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  final int days = 90;
+  final String name = "Omar";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Some Title",
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello World, $days, $name"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Hello World"),
-        ),
-      ),
-    ));
+    return const MaterialApp(home: HomePage());
   }
 }
